51931ca3652bcc6672e759dda65dfd4f3cd1638d
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-10-14 00:51:40 +0530

    new functionalities with pending tests

--- src/main/kotlin/data/basic/SignedInteger.kt
+++ src/main/kotlin/data/basic/SignedInteger.kt
@@ -1,98 +1,27 @@
 package org.example.data.basic
 
-import org.example.util.extensions.string.elongate
-import org.example.util.extensions.string.getIntOnIndexOrElse
 import org.example.util.extensions.string.isAPlainUnSignedInteger
-import kotlin.math.max
 
 class SignedInteger {
-
-
-    companion object {
-        fun String.removeInitialZeroes() = this.dropWhile { it == '0' }
-
-        private object Unsigned {
-            /** uses unsigned integers for addition
-             * @throws IllegalArgumentException if both of them don't have matching polarity
-             */
-            fun addition(
-                a: SignedInteger,
-                b: SignedInteger
-            ): SignedInteger {
-                if (a.isPositive == b.isPositive) {
-                    val totalLength = max(a = a.value.length, b = b.value.length)
-                    val first = a.value.elongate(length = totalLength)
-                    val second = b.value.elongate(length = totalLength)
-
-                    var result = ""
-                    var carry = 0
-                    ((totalLength - 1) downTo -1).forEach { index -> // -1 accommodates a carry
-                        (
-                                first.getIntOnIndexOrElse(index = index) +
-                                        second.getIntOnIndexOrElse(index = index) +
-                                        carry
-                                )
-                            .toString()
-                            .reversed()
-                            .let {
-                                result += it.getOrNull(index = 0)
-                                carry = it.getOrNull(index = 1)?.digitToIntOrNull() ?: 0
-                            }
-
-                    }
-                    return SignedInteger(
-                        value = result.reversed().removeInitialZeroes(),
-                        isPositive = a.isPositive
-                    )
-                } else {
-                    throw IllegalArgumentException("both numbers [$a] [$b] should be positive")
-                }
-            }
-
-            fun substraction(
-                a: SignedInteger,
-                b: SignedInteger
-            ): SignedInteger {
-                if (a.isPositive && b.isPositive) {
-                    val firstIsGreater = if (a.value.length == b.value.length) {
-                        (a.value.first().digitToIntOrNull() ?: 0) > (b.value.first().digitToIntOrNull() ?: 0)
-                    } else {
-                        a.value.length > b.value.length
-                    }
-
-                    val maxLength = max(a = a.value.length, b = b.value.length)
-                    val first = (if (firstIsGreater) a else b).value.elongate(length = maxLength)
-                    val second = (if (firstIsGreater) b else a).value.elongate(length = maxLength)
-                    TODO()
-                } else {
-                    throw IllegalArgumentException("both numbers [$a] [$b] should be positive")
-                }
-            }
-        }
-    }
-
     val isPositive: Boolean
-    val value: String
+    val value: UnSignedInteger
 
     constructor(value: String) {
-        val numbers = "1234567890"
-
         when {
             value.isEmpty() -> {
-                this.value = "0"
+                this.value = UnSignedInteger(value = "0")
                 this.isPositive = true
             }
 
             /** normal number without labelling */
             value.isAPlainUnSignedInteger() -> {
-                this.value = value
+                this.value = UnSignedInteger(value)
                 this.isPositive = true
-
             }
 
             /** number with labelling */
             value.first().let { it == '-' || it == '+' } && value.drop(n = 1).isAPlainUnSignedInteger() -> {
-                this.value = value.drop(n = 1)
+                this.value = UnSignedInteger(value = value.drop(n = 1))
                 this.isPositive = when (val first = value.first()) {
                     '-' -> false
                     '+' -> true
@@ -109,15 +38,55 @@ class SignedInteger {
         isPositive: Boolean,
     ) {
         if (value.isAPlainUnSignedInteger()) {
-            this.value = value
+            this.value = UnSignedInteger(value = value)
             this.isPositive = isPositive
         } else {
             throw IllegalArgumentException("value should be an unsigned integer")
         }
     }
 
+    constructor(
+        value: UnSignedInteger,
+        isPositive: Boolean,
+    ) {
+        this.value = value
+        this.isPositive = isPositive
+    }
+
     override fun toString(): String {
         return "${if (isPositive) "" else "-"}$value"
     }
 
+
+    companion object {
+        // TODO: check
+        fun normalAdditionOfSignedIntegers(
+            first: SignedInteger,
+            second: SignedInteger,
+        ): SignedInteger =
+            if (first.isPositive == second.isPositive) {
+                SignedInteger(
+                    value = (first.value * second.value),
+                    isPositive = first.isPositive
+                )
+            } else {
+                val firstIsBig = first.value >= second.value
+                SignedInteger(
+                    value = if (firstIsBig) {
+                        first.value - second.value
+                    } else {
+                        second.value - first.value
+                    },
+                    isPositive = if (firstIsBig) first.isPositive else second.isPositive
+                )
+            }
+    }
+
+    operator fun plus(other: SignedInteger): SignedInteger {
+        return normalAdditionOfSignedIntegers(first = this, second = other)
+    }
+
+    operator fun minus(other: SignedInteger): SignedInteger {
+        return normalAdditionOfSignedIntegers(first = this, second = other)
+    }
 }
