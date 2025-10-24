7d2f2706d16e5d6b10b5d045135529dd782ee752
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-10-24 23:10:44 +0530

    format

--- src/test/kotlin/integers/UnsignedIntegerTests.kt
+++ src/test/kotlin/integers/UnsignedIntegerTests.kt
@@ -3,14 +3,27 @@ package integers
 import org.example.data.basic.UnSignedInteger
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class UnsignedIntegerTests {
     @Test
     fun unsignedInitiation() {
-        assert(value = UnSignedInteger(value = "").value == "0")
-        assert(value = UnSignedInteger(value = "0").value == "0")
-        assert(value = UnSignedInteger(value = "1_000").value == "1000")
-        assert(value = UnSignedInteger(value = "0003").value == "0003")
+        assertEquals(
+            expected = "0",
+            actual = UnSignedInteger(value = "").value
+        )
+        assertEquals(
+            expected = "0",
+            actual = UnSignedInteger(value = "0").value
+        )
+        assertEquals(
+            expected = "1000",
+            actual = UnSignedInteger(value = "1_000").value
+        )
+        assertEquals(
+            expected = "3",
+            actual = UnSignedInteger(value = "0003").value
+        )
         listOf("12.3", "we", "+-2").forEach {
             assertThrows<IllegalArgumentException> { UnSignedInteger(value = it) }.printStackTrace()
         }
@@ -18,11 +31,20 @@ class UnsignedIntegerTests {
 
     @Test
     fun calculations() {
-        assert(value = (UnSignedInteger(value = "200") + UnSignedInteger(value = "453")).value == "653")
-        assert(value = (UnSignedInteger(value = "567") - UnSignedInteger(value = "123")).value == "444")
+        assertEquals(
+            expected = "653",
+            actual = (UnSignedInteger(value = "200") + UnSignedInteger(value = "453")).value
+        )
+        assertEquals(
+            expected = "444",
+            actual = (UnSignedInteger(value = "567") - UnSignedInteger(value = "123")).value
+        )
         assertThrows<IllegalStateException> {
             UnSignedInteger(value = "123") - UnSignedInteger(value = "567")
         }
-        assert(value = (UnSignedInteger(value = "45") * UnSignedInteger(value = "222")).value == "9990")
+        assertEquals(
+            expected = "9990",
+            actual = (UnSignedInteger(value = "45") * UnSignedInteger(value = "222")).value
+        )
     }
 }
